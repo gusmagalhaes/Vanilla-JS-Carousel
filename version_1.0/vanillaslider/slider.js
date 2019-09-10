0ce013f5061b41b94e907b19a01afce14b4269de
Gus Magalhaes <48729520+gusmagalhaes@users.noreply.github.com>
2019-09-10 12:32:22 -0400

    Update slider.js

--- version_1.0/vanillaslider/slider.js
+++ version_1.0/vanillaslider/slider.js
@@ -1,7 +1,32 @@
-//AUTOPLAY: Set "var autoplay = true" and a slidetime time:
-var autoplay, slidetime;
-if (autoplay === true){var start = setInterval(nextSlide, slidetime);}
- 
+//Set slide time interval:
+
+const slidetime = 3500;
+//https://stackoverflow.com/questions/8126466/how-do-i-reset-the-setinterval-timer  
+function Timer(fn, t) {
+    var timerObj = setInterval(fn, t);
+    this.stop = function() {
+        if (timerObj) {
+            clearInterval(timerObj);
+            timerObj = null;
+        }
+        return this;
+    }
+    // start timer using current settings (if it's not already running)
+    this.start = function() {
+        if (!timerObj) {
+            this.stop();
+            timerObj = setInterval(fn, t);
+        }
+        return this;
+    }
+    // start with new interval, stop current interval
+    this.reset = function(newT) {
+        t = newT;
+        return this.stop().start();
+    }
+}
+const timer = new Timer(function(){nextSlide();}, slidetime);
+
 //Button Next Slide
 
 function nextSlide(){
@@ -20,16 +45,11 @@ function nextSlide(){
       document.querySelectorAll(".slide-controller li")[0].className += ' active';
       document.getElementById("slide-1").className += ' active';
   }
-  clearInterval(start);
-	if (autoplay === true){
-			start = setInterval(nextSlide, slidetime);
-	}
+  timer.reset(slidetime);
   
 }
 const button_next = document.querySelectorAll(".arrow-button-right")[0];
 button_next.addEventListener('click', nextSlide);
-
-
 
 //Button Previous Slide
 
@@ -49,15 +69,10 @@ function previousSlide(){
       document.querySelectorAll(".slide-controller li")[0].className += ' active';
       document.getElementById("slide-1").className += ' active';
   }
-   clearInterval(start);
-	if (autoplay === true){
-			start = setInterval(nextSlide, slidetime);
-	}
+ timer.reset(slidetime);
 }
 const button_previous = document.querySelectorAll(".arrow-button-left")[0];
 button_previous.addEventListener('click', previousSlide);
-
-
 
 // Slide Circle Indicators
 
@@ -72,10 +87,7 @@ for (each of clicks) {
 			}
 			el.className += ' active';
 			document.getElementById(el.dataset.link).className += ' active';
-  clearInterval(start);
-	if (autoplay === true){
-			start = setInterval(nextSlide, slidetime);
-	}
+ timer.reset(slidetime);
 	});
 
 }
